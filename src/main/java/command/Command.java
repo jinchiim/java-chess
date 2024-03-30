ed0500f0dbb2b527c842c98615063605784167ac
Eugene Jang <kyunellroll@gmail.com>
2024-03-30 22:26:57 +0900

    feat(Command): status 커멘드 추가
    - Status가 일치하는지 확인하는 테스트 구현

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -4,7 +4,8 @@ public enum Command {
 
     START("start"),
     MOVE("move"),
-    END("end");
+    END("end"),
+    STATUS("status");
 
     private final String identifier;
 
@@ -16,12 +17,20 @@ public enum Command {
         return START.identifier.equals(identifier);
     }
 
+    public static boolean
+    isStatusCommand(String identifier) {
+        return STATUS.identifier.equals(identifier);
+    }
+
     public static boolean isCommandMove(String commandIdentifier) {
         if (isEndCommand(commandIdentifier)) {
             return false;
         }
         if (isStartCommand(commandIdentifier)) {
             throw new IllegalArgumentException("이미 시작한 상태 입니다.");
+        }
+        if (isStatusCommand(commandIdentifier)) {
+            return false;
         }
         return true;
     }
