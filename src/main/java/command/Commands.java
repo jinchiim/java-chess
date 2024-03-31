6bd4fdeace0f9416672c35f90824b6046ff3793e
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 01:44:30 +0900

    feat(Commands): 커맨드와 행동을 연결해주는 Commands 생성
    - 일치하는 커맨드인지 확인하는 메서드 테스트

---
+++ src/main/java/command/Commands.java
@@ -0,0 +1,28 @@
+package command;
+
+import java.util.Arrays;
+
+public enum Commands {
+
+    START("start", new Start()),
+    MOVE("move", new Move()),
+    END("end", new End()),
+    STATUS("status", new Status());
+
+    private final String identifier;
+    private final Command commandStatus;
+
+    Commands(String identifier, Command commandStatus) {
+        this.identifier = identifier;
+        this.commandStatus = commandStatus;
+    }
+
+    public static Command findCommand(String inputCommand) {
+        Commands commands = Arrays.stream(values())
+                .filter(command -> command.identifier.equals(inputCommand))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 Command가 없습니다."));
+
+        return commands.commandStatus;
+    }
+}
---
+++ src/test/java/domain/command/CommandsTest.java
@@ -0,0 +1,21 @@
+package domain.command;
+
+import command.Commands;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CommandsTest {
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"play", "stop", "go", "moving"})
+    @DisplayName("존재하지 않는 Command일 경우, 에러를 발생한다.")
+    void notExistCommandFind(String inputCommand) {
+        Assertions.assertThatThrownBy(() -> Commands.findCommand(inputCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당하는 Command가 없습니다.");
+    }
+
+}
