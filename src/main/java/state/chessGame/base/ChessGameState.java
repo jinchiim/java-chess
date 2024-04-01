3b9435130854922886aa89505727170c7a2d5066
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 17:21:22 +0900

    refactor(ChessGameState): 사용하지 않는 method 삭제

--- src/main/java/state/chessGame/base/ChessGameState.java
+++ src/main/java/state/chessGame/base/ChessGameState.java
@@ -1,7 +1,6 @@
 package state.chessGame.base;
 
 import domain.coordinate.Coordinate;
-import domain.piece.Color;
 import domain.piece.base.ChessPiece;
 import java.sql.SQLException;
 import java.util.Map;
@@ -25,6 +24,4 @@ public interface ChessGameState {
     Long getGameId();
 
     void show();
-
-    Color getTurn();
 }
