0a6db5a3217287017a06ec39cd5df36c3a9defd4
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 16:45:55 +0900

    refactor(ChessGameService): 체스 게임의 도메인 로직 Service 내부로 이동

--- src/main/java/service/ChessGameService.java
+++ src/main/java/service/ChessGameService.java
@@ -3,7 +3,15 @@ package service;
 import db.dao.ChessGameDao;
 import db.dao.ChessGameRepository;
 import db.entity.ChessGame;
+import domain.chessboard.ChessBoardScoreCalculator;
+import domain.coordinate.Coordinate;
+import domain.piece.Color;
+import domain.piece.base.ChessPiece;
 import java.sql.SQLException;
+import java.util.Map;
+import service.dto.GameScoreDto;
+import state.chessGame.base.ChessGameState;
+import state.chessGame.statusfactory.ChessStatusFactory;
 
 public class ChessGameService {
 
@@ -13,12 +21,25 @@ public class ChessGameService {
         this.chessGameRepository = new ChessGameDao();
     }
 
-    public Long addChessGame() throws SQLException {
+    public ChessGameState addChessGame() throws SQLException {
         ChessGame chessGame = ChessGame.create();
-        return chessGameRepository.save(chessGame);
+        Long id = chessGameRepository.save(chessGame);
+
+        return ChessStatusFactory.makeRunningChessGame(id);
     }
 
-    public void deleteChessGame(Long gameId) throws SQLException {
+    public ChessGameState deleteChessGame(Long gameId, ChessGameState chessGameState) throws SQLException {
+        ChessGameState endState = chessGameState.end();
         chessGameRepository.delete(gameId);
+        return endState;
+    }
+
+    public GameScoreDto calculateScore(ChessGameState chessGameState) {
+        Map<Coordinate, ChessPiece> board = chessGameState.getBoard();
+
+        double blackScore = ChessBoardScoreCalculator.calculate(Color.BLACK, board);
+        double whiteScore = ChessBoardScoreCalculator.calculate(Color.WHITE, board);
+
+        return new GameScoreDto(blackScore, whiteScore);
     }
 }
