e779a3f9e80c74a25e425bc23839778f50822599
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 17:17:38 +0900

    build(Docker-compose): 초기 데이터베이스 설정 파일 추가

---
+++ docker/db/mysql/init/init.sql
@@ -0,0 +1,20 @@
+USE chess;
+
+CREATE TABLE IF NOT EXISTS chess_game
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    is_running BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS piece
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    game_id    BIGINT NOT NULL,
+    color      VARCHAR(5),
+    piece_type VARCHAR(10),
+    `row`      VARCHAR(1),
+    `column`   VARCHAR(1),
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) references chess_game (id)
+);
