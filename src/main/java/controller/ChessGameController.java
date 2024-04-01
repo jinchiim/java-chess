48829cec2469ce2b565725f11ca59f5f0181784c
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 15:31:24 +0900

    refactor(ChessGameController): Service 의존성 추가
    - 최종 게임 완료 후 delete 메서드를 호출하도록 구현

--- src/main/java/controller/ChessGameController.java
+++ src/main/java/controller/ChessGameController.java
@@ -1,30 +1,38 @@
 package controller;
 
-import command.Command;
-import command.Commands;
+import command.base.Command;
+import command.base.Commands;
 import domain.chessGame.base.ChessGame;
 import domain.chessGame.statusfactory.ChessStatusFactory;
+import java.sql.SQLException;
 import java.util.List;
+import service.ChessGameService;
+import service.PieceService;
 import view.InputView;
 import view.OutputView;
 
 public class ChessGameController {
 
-    private final InputView inputView;
+    private final ChessGameService chessGameService;
+    private final PieceService pieceService;
 
-    public ChessGameController(InputView inputView) {
-        this.inputView = inputView;
+    public ChessGameController(ChessGameService chessGameService, PieceService pieceService) {
+        this.chessGameService = chessGameService;
+        this.pieceService = pieceService;
     }
 
-    public void run() {
+    public void run() throws SQLException {
         ChessGame chessGame = ChessStatusFactory.initChessGame();
         OutputView.printGameGuide();
         do {
-            List<String> inputCommand = inputView.receiveCommands();
+            List<String> inputCommand = InputView.receiveCommands();
             Command command = Commands.findCommand(inputCommand.get(0));
 
-            chessGame = command.execute(chessGame, inputCommand);
+            chessGame = command.execute(chessGame, inputCommand, pieceService);
             chessGame.show();
         } while (chessGame.isPlaying());
+
+        pieceService.deletePieces(chessGame.getGameId());
+        chessGameService.deleteChessGame(chessGame.getGameId());
     }
 }
