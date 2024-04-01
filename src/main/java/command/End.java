ae4b1cb3a10a3db58b70aa4c431c6bf126a3a4fa
Eugene Jang <kyunellroll@gmail.com>
2024-04-01 17:22:30 +0900

    style(End): 인라인으로 수정

--- src/main/java/command/End.java
+++ src/main/java/command/End.java
@@ -12,7 +12,6 @@ public class End implements Command {
     @Override
     public ChessGameState execute(ChessGameService chessGameService, List<String> inputCommand,
                                   PieceService pieceService, ChessGameState chessGameState) throws SQLException {
-        ChessGameState endState = chessGameService.stopChessGame(chessGameState);
-        return endState;
+        return chessGameService.stopChessGame(chessGameState);
     }
 }
