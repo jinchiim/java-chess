feec35bb8a976dd44069bbcbc2bfb75febe16cef
Eugene Jang <kyunellroll@gmail.com>
2024-04-05 03:33:46 +0900

    feat(ChessGameDao): 체스 방의 이름으로 체스 아이디를 가쟈오는 메서드 구현

--- src/main/java/db/dao/ChessGameDao.java
+++ src/main/java/db/dao/ChessGameDao.java
@@ -1,5 +1,6 @@
 package db.dao;
 
+import db.booleanTranslator.BooleanTranslator;
 import db.connection.DatabaseConnection;
 import db.entity.ChessGame;
 import java.sql.Connection;
@@ -12,23 +13,22 @@ public class ChessGameDao {
 
     public Long save(ChessGame chessGame) throws SQLException {
         String query = "INSERT INTO chess_game " +
-                "(id, is_running) " +
-                "VALUES(DEFAULT, true)";
+                "(id, is_running, room_name) " +
+                "VALUES(DEFAULT, true, ?)";
         Connection connection = DatabaseConnection.getConnection();
 
         try {
             connection.setAutoCommit(false);
             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, chessGame.getRoomName());
             statement.executeUpdate();
 
             ResultSet result = statement.getGeneratedKeys();
 
             if (result.next()) {
                 connection.commit();
-                connection.close();
                 return result.getLong(1);
             }
-            connection.rollback();
             connection.close();
             throw new RuntimeException("해당하는 데이터가 없습니다.");
         } catch (SQLException e) {
@@ -48,10 +48,8 @@ public class ChessGameDao {
             connection.setAutoCommit(false);
             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             statement.setLong(1, gameId);
-
             statement.executeUpdate();
             connection.commit();
-            connection.close();
         } catch (SQLException e) {
             connection.rollback();
             connection.close();
@@ -73,11 +71,45 @@ public class ChessGameDao {
             statement.executeUpdate();
 
             connection.commit();
-            connection.close();
+
         } catch (SQLException e) {
             connection.rollback();
             connection.close();
             throw new RuntimeException("데이터베이스 생성에 실패했습니다." + e.getMessage());
+        }
+    }
+
+    public Long findChessGameByName(String roomName) throws SQLException {
+        String query = "SELECT * " +
+                "FROM chess_game " +
+                "WHERE room_name = ?";
+
+        Connection connection = DatabaseConnection.getConnection();
+
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, roomName);
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                connection.commit();
+                validateNotEndGame(result);
+                return result.getLong(1);
+            }
+            connection.close();
+
+            throw new RuntimeException("해당하는 데이터가 없습니다.");
+        } catch (SQLException e) {
+            connection.rollback();
+            connection.close();
+            throw new RuntimeException("체스 게임 불러오기에 실패했습니다." + e.getMessage());
+        }
+    }
+
+    private static void validateNotEndGame(ResultSet result) throws SQLException {
+        if (result.getInt(3) == BooleanTranslator.translate(false)) {
+            throw new IllegalArgumentException("이미 종료된 게임입니다.");
         }
     }
 }
