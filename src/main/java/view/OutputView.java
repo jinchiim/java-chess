b1acb58fc6880335a3b51f292ef6c51a67a8cb86
Eugene Jang <kyunellroll@gmail.com>
2024-04-05 03:37:46 +0900

    feat(OutputView): 방 이름을 불러오는 예시 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -19,7 +19,7 @@ public class OutputView {
         System.out.print("""
                 > 체스 게임을 시작합니다.
                 > 새로운 게임 시작 : start
-                > 게임 이어서 시작 : load 방 이름
+                > 게임 이어서 시작 : load 방 이름 - 예. load pollaChess
                 > 게임 종료 : end
                 > 게임 이동 : move source위치 target위치 - 예. move b2 b3
                 > 게임 점수 : status
