87f1214165098f70127647b42cf5836e7cec7525
Eugene Jang <kyunellroll@gmail.com>
2024-04-05 09:30:38 +0900

    refactor(ChessGame): 턴 필드 추가 및 턴을 가져오는 메서드 추가

--- src/main/java/db/entity/ChessGame.java
+++ src/main/java/db/entity/ChessGame.java
@@ -6,17 +6,23 @@ import domain.room.GameRoom;
 public class ChessGame {
     private final int isRunning;
     private final String roomName;
+    private final String turn;
 
-    private ChessGame(int isRunning, String roomName) {
+    public ChessGame(int isRunning, String roomName, String turn) {
         this.isRunning = isRunning;
         this.roomName = roomName;
+        this.turn = turn;
     }
 
-    public static ChessGame create(GameRoom gameRoom) {
-        return new ChessGame(BooleanTranslator.translate(false), gameRoom.getName());
+    public static ChessGame create(GameRoom gameRoom, String turn) {
+        return new ChessGame(BooleanTranslator.translate(false), gameRoom.getName(), turn);
     }
 
     public String getRoomName() {
         return roomName;
+    }
+
+    public String getTurn() {
+        return turn;
     }
 }
